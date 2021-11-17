54ec1880ecac7d750a15ef8ed02c56e118ebeeb2
denis <denis.mourier2@gmail.com>
2021-11-17 15:27:21 +0100

    tp OOP J7-J8 auto-Recensement: menu appli

---
+++ src/fr/diginamic/recensement/Application.java
@@ -0,0 +1,69 @@
+package fr.diginamic.recensement;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args)throws IOException {
+		// chargement du fichier :
+		
+		Path fichierRecensement = Paths.get("d:/workspace-spring-diginamic/approche-objet/src/fr/diginamic/recensement/recensement.csv");
+		List<String> linesFichierRecensement = Files.readAllLines(fichierRecensement);
+		Recensement recensement = new Recensement();
+		for (String line : linesFichierRecensement) {
+			recensement.ajouteVille(line);
+		}
+		
+		
+		
+
+		System.out.println();
+		System.out.println("votre choix : ");
+
+		System.out.println("1. Population d’une ville donnée");
+		System.out.println("2. Population d’un département donné");
+		System.out.println("3. Population d’une région donnée");
+		System.out.println("4. Afficher les 10 régions les plus peuplées");
+		System.out.println("5. Afficher les 10 départements les plus peuplés");
+		System.out.println("6. Afficher les 10 villes les plus peuplées d’un département");
+		System.out.println("7. Afficher les 10 villes les plus peuplées d’une région");
+		System.out.println("8. Afficher les 10 villes les plus peuplées de France");
+		System.out.println("9. Sortir");
+
+		System.out.println();
+
+		Scanner scanner = new Scanner(System.in);
+//		int nb = scanner.nextInt();
+
+		while (true) {
+		int	nb = scanner.nextInt();
+
+			switch (nb) {
+			case 1:
+				RecherchePopulationVille recherche = new RecherchePopulationVille(recensement);
+				recherche.traiter();
+				break;
+				
+			case 2:
+				RecherchePopulationDepartement recherche = new RecherchePopulationDepartement(recensement);
+				recherche.traiter();
+				break;
+			case 9:
+				System.out.println("good bye !");
+				System.exit(0);
+				break;
+
+			default:
+				System.out.println("good bye !");
+				System.exit(0);
+				break;
+			}
+		}
+	}
+
+}
